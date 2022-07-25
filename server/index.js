0994d821071ea924f5b36ee946887d16bf573653
Sara Jamal <sara.jamal@10pearls.com>
2022-07-25 16:46:35 +0500

    removed routes

--- server/index.js
+++ server/index.js
@@ -13,23 +13,6 @@ app.use(express.json());                          // parse requests of content-t
 app.use(express.urlencoded({ extended: true }));  // parse requests of content-type - application/x-www-form-urlencoded
 
 
-// // routes
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome to Home Services Web application." });
-// });
-
-// app.get("/admin", (req, res) => {
-//   // res.json({ message: "Hello from server." });
-//   db.query("SELECT * from admin", (err,rows,fields) => {
-//     if (!err)
-//       res.send(rows);
-//     else
-//       console.log(err);
-
-//   });
-// });
-
-
 // set port, listen for requests
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
