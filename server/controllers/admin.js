41c181faf91019c2a67c0d347babee472acdeb33
Ahmed Naeem <ahmed.naeem@10pearls.com>
2022-07-25 17:43:42 +0500

    backend(user & admin login/signup APIs added)

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -5,13 +5,26 @@ exports.login = async (req, res)=>{
     const admin = req.body;
     const username = admin.username;
     const password = admin.password;
-    let sql = `SELECT * FROM home_services.ADMIN WHERE USERNAME = '${username}' AND PASSWORD = '${password}'`;
-    db.query(sql, (error, results, fields)=>{
-        if (error) throw error;
-        if(results){
-            res.send(results[0]);
-        }else{
-            res.status(404).send('failed');
+    let sql = `SELECT * FROM ADMIN WHERE USERNAME = '${username}' AND PASSWORD = '${password}'`;
+    db.query(sql, (error, results, fields) => {
+        if (error){
+            res.status(400).json({
+                status: 400,
+                message: error.sqlMessage
+            })
+        }
+        else{
+            if(results.length){
+                res.status(200).json({
+                    status: 200,
+                    data: results[0]
+                });
+            }else{
+                res.status(404).json({
+                    status: 404,
+                    message: "Invalid username or password!"
+                });
+            }
         }
     });
 }
@@ -19,5 +32,20 @@ exports.login = async (req, res)=>{
 exports.register = async (req, res)=>{
     const db = dbo.connect();
     const admin = req.body;
-   
+    const username = admin.username;
+    const password = admin.password;
+    let sql = `INSERT INTO ADMIN(username, password) VALUES('${username}','${password}')`
+    db.query(sql, (error, result) => {
+        if (error){
+            res.status(400).json({
+                status: 400,
+                message: error.sqlMessage
+            })
+        }else{
+            res.status(200).json({
+                status: 200,
+                message: "Admin successfully registered!"
+            })
+        }
+    });
 }
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,12 +5,26 @@ exports.login = async (req, res)=>{
     const user = req.body;
     const username = user.username;
     const password = user.password;
-    let sql = `SELECT * FROM HOME_SERVICES.CUSTOMER WHERE USERNAME = '${username}' AND PASSWORD = '${password}'`;
-    db.query(sql, (error, fields, results)=>{
-        if(results){
-            res.send(results[0]);
-        }else{
-            res.status(404).send('failed');
+    let sql = `SELECT * FROM CUSTOMER WHERE USERNAME = '${username}' AND PASSWORD = '${password}'`;
+    db.query(sql, (error, results, fields)=>{
+        if (error){
+            res.status(400).json({
+                status: 400,
+                message: error.sqlMessage
+            })
+        }
+        else{
+            if(results.length){
+                res.status(200).json({
+                    status: 200,
+                    data: results[0]
+                });
+            }else{
+                res.status(404).json({
+                    status: 404,
+                    message: "Invalid username or password!"
+                });
+            }
         }
     });
 }
@@ -18,5 +32,27 @@ exports.login = async (req, res)=>{
 exports.register = async (req, res)=>{
     const db = dbo.connect();
     const user = req.body;
-    
+
+    // setting primary info for now
+    const username = user.username;
+    const password = user.password;
+    const first_name = user.first_name;
+    const last_name = user.last_name;
+    const email = user.email;
+    const phone = user.phone;
+
+    let sql = `INSERT INTO CUSTOMER(username, password, first_name, last_name, email, phone) VALUES('${username}','${password}','${first_name}','${last_name}','${email}','${phone}')`;
+    db.query(sql, (error, result) => {
+        if (error){
+            res.status(400).json({
+                status: 400,
+                message: error.sqlMessage
+            })
+        }else{
+            res.status(200).json({
+                status: 200,
+                message: "User successfully registered!"
+            })
+        }
+    });
 }
