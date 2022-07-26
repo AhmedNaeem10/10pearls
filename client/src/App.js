b0d531c384bbf504901dfd87028a7ce79b9129c1
harisaltaf45 <haris.altaf@10pearls.com>
2022-07-26 15:01:20 +0500

    login and Sign Up pages ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
 import AdminLogin from "./components/AdminLogin";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import UserLogin from "./components/UserLogin";
+import Navbar from "./components/Navbar";
+import UserSignUp from "./components/UserSignUp";
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/" exact element={<UserLogin />} />
+        <Route path="/" exact element={<UserSignUp />} />
+        <Route path="/signup" exact element={<UserLogin />} />
       </Routes>
     </div>
   );
