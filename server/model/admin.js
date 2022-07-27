e25b7c68c844c6a805972f2be81f34bbc63e3583
Ahmed Naeem <ahmed.naeem@10pearls.com>
2022-07-27 11:46:13 +0500

    plural

---
+++ server/model/admin.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, Sequelize) => {
+    return TEST = sequelize.define("ADMINS", {
+        id: {
+            type: Sequelize.INTEGER,
+            field: "id",             
+            autoIncrement: 1,       
+            primaryKey: 1
+        },
+        TEST: {
+            type: Sequelize.INTEGER
+        },
+        PARA: {
+            type: Sequelize.TEXT
+        }, 
+    }, {
+        timestamps: false,
+
+        // If don't want createdAt
+        createdAt: false,
+
+        // If don't want updatedAt
+        updatedAt: false,
+    });
+};
