617617ff40af763af87254e168616a330b98c838
Ahmed Naeem <ahmed.naeem@10pearls.com>
2022-08-17 15:33:21 +0500

    done

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.json());
+// app.use(express.json());
 // app.use(middleware.decodeToken);
 
 const db = require("./model");
